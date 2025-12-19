46aa97ae11d34c3017205d174081fb050bf20c9f
José Vitor <jose-vitor-7@hotmail.com>
2025-12-18 22:13:50 -0300

    feat: add responsive menu

--- src/pages/_app/layout.tsx
+++ src/pages/_app/layout.tsx
@@ -1,9 +1,12 @@
 import { createFileRoute, Link, Outlet, useNavigate } from '@tanstack/react-router'
 import { isAxiosError } from 'axios'
+import { CookingPot, Menu, Plus } from 'lucide-react'
 import { useEffect, useMemo } from 'react'
+
 import { ThemeToggle } from '@/components/theme-toggle'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
+import { Sheet, SheetClose, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet'
 import { api } from '@/lib/axios'
 import { useAuthStore } from '@/stores/auth'
 
@@ -62,7 +65,7 @@ function RouteComponent() {
 		<div className="w-full h-screen overflow-hidden flex flex-col">
 			<nav className="flex justify-between w-full p-4 items-center">
 				<h1 className="text-2xl font-bold">Recipeez</h1>
-				<div className="flex items-center gap-4">
+				<div className="hidden md:flex items-center gap-4">
 					<Link to="/recipes">Receitas</Link>
 					<Link to="/recipes/new">Nova Receita</Link>
 					<ThemeToggle />
@@ -73,6 +76,40 @@ function RouteComponent() {
 					<Button type="button" variant="destructive" onClick={handleSignOut}>
 						Sair
 					</Button>
+				</div>
+				<div className="md:hidden">
+					<Sheet>
+						<SheetTrigger asChild>
+							<Button variant="outline" size="icon">
+								<Menu className="size-4" />
+							</Button>
+						</SheetTrigger>
+						<SheetContent className="w-full">
+							<SheetHeader>
+								<SheetTitle>Menu</SheetTitle>
+							</SheetHeader>
+							<div className="flex flex-col justify-between h-full p-4">
+								<div className="flex flex-col gap-6">
+									<SheetClose asChild>
+										<Link to="/recipes" className="flex items-center gap-2">
+											<CookingPot className="size-4" />
+											Receitas
+										</Link>
+									</SheetClose>
+									<SheetClose asChild>
+										<Link to="/recipes/new" className="flex items-center gap-2">
+											<Plus className="size-4" />
+											Nova Receita
+										</Link>
+									</SheetClose>
+									<Button type="button" variant="destructive" onClick={handleSignOut}>
+										Sair
+									</Button>
+								</div>
+								<ThemeToggle />
+							</div>
+						</SheetContent>
+					</Sheet>
 				</div>
 			</nav>
 			<div className="w-full h-full overflow-x-hidden overflow-y-auto flex-1">
